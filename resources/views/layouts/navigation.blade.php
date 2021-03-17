4b68e2bf103fe1e5e5cdc84d33bb7dbe81e0b139
Hlaing Min Than(Faizal) <hlaingminthan92@gmail.com>
2021-03-17 23:43:08 +0630

    fix some style

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -3,17 +3,17 @@
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
             <div class="flex">
-                <!-- Logo -->
-                <div class="flex-shrink-0 flex items-center">
-                    <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-10 w-auto fill-current text-gray-600" />
-                    </a>
-                </div>
+              
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
+                    <x-nav-link :href="route('threads.index')" :active="request()->routeIs('dashboard')" class="text-2xl">
+                        Forum App
+                    </x-nav-link>
+                </div>
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-nav-link :href="route('threads.create')" :active="request()->routeIs('dashboard')" class="text-2xl">
+                        Add New Thread
                     </x-nav-link>
                 </div>
             </div>
--- resources/views/threads/index.blade.php
+++ resources/views/threads/index.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight text-center">
             All Threads
         </h2>
     </x-slot>
