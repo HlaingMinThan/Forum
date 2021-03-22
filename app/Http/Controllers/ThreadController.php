f9c3010dd37595e66b67d1be0add8d662a15feb2
Hlaing Min Than(Faizal) <hlaingminthan92@gmail.com>
2021-03-22 21:39:56 +0630

    basic refactoring

--- app/Http/Controllers/ThreadController.php
+++ app/Http/Controllers/ThreadController.php
@@ -13,30 +13,16 @@ class ThreadController extends Controller
     public function __construct(){
         $this->middleware("auth")->except('index','show');
     }
+    
     public function index($channelSlug=null){
-        if(!empty($channelSlug)){
-            $channelId=Channel::whereSlug($channelSlug)->firstOrFail()->id;
-            $threads=Thread::where("channel_id",$channelId)->latest();
-        }else{
-            $threads=Thread::latest();
-        }
-        if($username=request('by')){
-            $userId=User::where("name",$username)->firstOrFail()->id;
-            $threads=Thread::where("user_id",$userId)->latest();
-        }
-        if(request('popular')){
-            $threads = Thread::orderBy("replies_count","desc");
-        }
-        if(request('unanswered')){
-            $threads = Thread::where("replies_count",0);
-        }
-           $threads=$threads->get();
+        $threads=$this->filterThreads($channelSlug);
         return view("threads.index",compact('threads'));
     }
 
     public function create(){
         return view('threads.create');
     }
+
     public function store(){
         request()->validate([
             'title'=>"required",
@@ -51,12 +37,14 @@ class ThreadController extends Controller
         ]);
         return redirect($thread->path())->with("flash","A Thread Has Been Created");
     }
+
     public function show($channelSlug,Thread $thread){
         return view("threads.show",[
             'thread'=>$thread,
             'replies'=>$thread->replies()->with('owner')->withCount('favorites')->latest()->paginate(5)
         ]);
     }
+
     public function destroy($channelSlug=null,Thread $thread){
         $this->authorize('update',$thread);
         /* 
@@ -77,5 +65,25 @@ class ThreadController extends Controller
         */
         $thread->delete();
         return redirect()->route("threads.index");
+    }
+
+    protected  function filterThreads($channelSlug=null){
+        if(!empty($channelSlug)){
+            $channelId=Channel::whereSlug($channelSlug)->firstOrFail()->id;
+            $threads=Thread::where("channel_id",$channelId)->latest();
+        }else{
+            $threads=Thread::latest();
+        }
+        if($username=request('by')){
+            $userId=User::where("name",$username)->firstOrFail()->id;
+            $threads=Thread::where("user_id",$userId)->latest();
+        }
+        if(request('popular')){
+            $threads = Thread::orderBy("replies_count","desc");
+        }
+        if(request('unanswered')){
+            $threads = Thread::where("replies_count",0);
+        }
+          return $threads=$threads->get();
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -20,22 +20,26 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+// Thread
 Route::get("/threads",[ThreadController::class,'index'])->name('threads.index');
 Route::get("/threads/create",[ThreadController::class,"create"])->name('threads.create');
-Route::get("/threads/{channel}",[ThreadController::class,'index'])->name('channel.index');
 Route::post("/threads/store",[ThreadController::class,"store"])->name('threads.store');
 Route::get("/threads/{channel}/{thread}",[ThreadController::class,'show'])->name('threads.show');
 Route::delete("/threads/{channel}/{thread}",[ThreadController::class,'destroy'])->name('threads.destroy');
+Route::get("/threads/{channel}",[ThreadController::class,'index'])->name('channel.index');
+
+// Reply
 Route::get("/threads/{channel}/{thread}/replies",[RepliesController::class,"index"])->name('replies.index');
 Route::post("/threads/{channel}/{thread}/replies",[RepliesController::class,"store"])->name('replies.store');
 Route::delete("/replies/{reply}",[RepliesController::class,"destroy"])->name('replies.destroy');
 Route::patch("/replies/{reply}",[RepliesController::class,"update"])->name('replies.update');
+
+//Favorite
 Route::post("/replies/{reply}/favorite",[FavoriteController::class,"store"])->name('favorites.store');
 Route::delete("/replies/{reply}/favorite",[FavoriteController::class,"destroy"])->name('favorites.destroy');
+
+//Profile
 Route::get('/profiles/{user}',[ProfilesController::class,'show'])->name("profiles.show");
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
 
 require __DIR__.'/auth.php';
