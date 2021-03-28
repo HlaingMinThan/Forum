96e836aa08eff336eefbff121214edecb63030f6
Hlaing Min Than(Faizal) <hlaingminthan92@gmail.com>
2021-03-28 15:42:17 +0630

    trending threads should show if it's exists

--- resources/views/threads/index.blade.php
+++ resources/views/threads/index.blade.php
@@ -45,14 +45,16 @@
                 {{$threads->links()}}
         </div>
         <div class="">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg mb-5 p-5 font-semibold">
-                <h1 class=" text-2xl text-center ">Current Trending Threads</h1>
-                <ul class="list-disc list-inside mt-5">
-                    @foreach ($trending_threads as $thread)
+            @if (count($trending_threads))
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg mb-5 p-5 font-semibold">
+                    <h1 class=" text-2xl text-center ">Current Trending Threads</h1>
+                    <ul class="list-disc list-inside mt-5">
+                        @foreach ($trending_threads as $thread)
                         <li class="bg-gray-200 p-3 border border-gray-300 text-lg"><a href="{{$thread->path}}">{{$thread->title}}</a></li>
-                    @endforeach
-                </ul>
-            </div>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
         </div>
     </div>
 </x-app-layout>
